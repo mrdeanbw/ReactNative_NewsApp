d5fd73c814cad7db2a078f5d2e69ee31001e3d47
greendream1216 <udacitywww1216@gmail.com>
2019-12-23 22:03:20 +0700

    Add components

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+//FireBase -- REPLACE WITH YOUR VALUES
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyCH6wmL18AbDqCIXNsGBGINxzUnlPkB3bs",
+  authDomain: "universalapp-a272a.firebaseapp.com",
+  databaseURL: "https://universalapp-a272a.firebaseio.com",
+  projectId: "universalapp-a272a",
+  storageBucket: "universalapp-a272a.appspot.com",
+  messagingSenderId: "35001014547"
+};
+  
+exports.config = firebaseConfig;
